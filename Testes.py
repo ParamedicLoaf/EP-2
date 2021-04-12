e1669447c3d8c25586727e4a614f43b538ff65e5
Thiago Santos <tynamospark@gmail.com>
2021-04-12 18:03:24 -0300

    Primeiro commit (Thiago Teixeira)

---
+++ Testes.py
@@ -0,0 +1,16 @@
+import random
+
+l = [0, 'bolacha', 2, 'bola', 4]
+print(l)
+# [0, 1, 2, 3, 4]
+
+random.shuffle(l)
+print(l)
+# [4, 3, 2, 1, 0]
+
+print("\033[1;32;40m4") #Verde
+print("\033[1;31;40m4") #Vermelho
+print("\033[1;35;40m4") #Roxo
+print("\033[1;33;40m4") #Amarelo
+print("\033[0;37;40m4") #Normal
+
